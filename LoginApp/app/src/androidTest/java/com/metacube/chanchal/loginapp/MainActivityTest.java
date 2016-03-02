c5fedbb1597e9943be388a10f289b30aaac0988c
chanchaldabriya7 <chanchal.dabriya@metacube.com>
2016-03-02 18:56:29 +0530

    Unit test example added

--- LoginApp/app/src/androidTest/java/com/metacube/chanchal/loginapp/MainActivityTest.java
+++ LoginApp/app/src/androidTest/java/com/metacube/chanchal/loginapp/MainActivityTest.java
@@ -1,21 +1,51 @@
 package com.metacube.chanchal.loginapp;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.AndroidTestCase;
+import android.test.AndroidTestRunner;
+import android.test.suitebuilder.annotation.SmallTest;
 import android.widget.TextView;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
 /**
  * Created by Chanchal on 01-03-2016.
  */
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-        MainActivity activity;
-        MainActivityTest() {
-            super(MainActivity.class);
-            activity = getActivity();
-        }
-        @Test
-        public void usernameTextValidator() {
-            TextView username= (TextView) activity.findViewById(R.id.uname);
-            assertNotNull("Field cannot be null", username);
-        }
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    MainActivity activity;
+    public MainActivityTest(Class<MainActivity> activityClass) {
+        super(activityClass);
+    }
+
+    @Before
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        activity = getActivity();
+    }
+
+    @Test
+    public void usernameTextValidator() throws Exception {
+        System.out.println("1st statement");
+        TextView username= (TextView) activity.findViewById(R.id.uname);
+        System.out.println("2nd statement");
+        assertNotNull("Field cannot be null", username);
+        System.out.println("3rd statement");
+    }
+
+    @After
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 }
--- LoginApp/app/src/test/java/com/metacube/chanchal/loginapp/ExampleUnitTest.java
+++ LoginApp/app/src/test/java/com/metacube/chanchal/loginapp/ExampleUnitTest.java
@@ -12,4 +12,14 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void loginCheckTest() {
+        MainActivity obj = new MainActivity();
+        assertTrue( "Login is successful",obj.loginCheck("chanchal","dabriya") );
+    }
+    @Test
+    public void loginCheckTestTwo() {
+        MainActivity obj = new MainActivity();
+        assertEquals( "Login is successful",obj.loginCheck("chanchal","dabriya"),true);
+    }
 }

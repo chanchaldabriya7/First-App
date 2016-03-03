5b11e81ed76aeeed4c9261b38c24e71f5b499cc3
chanchaldabriya7 <chanchal.dabriya@metacube.com>
2016-03-03 11:44:51 +0530

    Instrument test-1

--- LoginApp/app/src/androidTest/java/com/metacube/chanchal/loginapp/MainActivityTest.java
+++ LoginApp/app/src/androidTest/java/com/metacube/chanchal/loginapp/MainActivityTest.java
@@ -3,9 +3,6 @@ package com.metacube.chanchal.loginapp;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.AndroidTestCase;
-import android.test.AndroidTestRunner;
-import android.test.suitebuilder.annotation.SmallTest;
 import android.widget.TextView;
 
 import org.junit.After;
@@ -22,13 +19,13 @@ import static org.junit.Assert.*;
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity>{
 
     MainActivity activity;
-    public MainActivityTest(Class<MainActivity> activityClass) {
-        super(activityClass);
+    public MainActivityTest() {
+        super(MainActivity.class);
     }
 
     @Before
     @Override
-    protected void setUp() throws Exception {
+    public void setUp() throws Exception {
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         activity = getActivity();
@@ -36,16 +33,13 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
     @Test
     public void usernameTextValidator() throws Exception {
-        System.out.println("1st statement");
         TextView username= (TextView) activity.findViewById(R.id.uname);
-        System.out.println("2nd statement");
         assertNotNull("Field cannot be null", username);
-        System.out.println("3rd statement");
     }
 
     @After
     @Override
-    protected void tearDown() throws Exception {
+    public void tearDown() throws Exception {
         super.tearDown();
     }
 }

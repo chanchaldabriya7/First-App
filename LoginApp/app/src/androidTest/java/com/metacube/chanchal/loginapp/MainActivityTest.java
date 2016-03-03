252483132a9ca254a5608b6b3013c4f09874e637
chanchaldabriya7 <chanchal.dabriya@metacube.com>
2016-03-03 19:26:02 +0530

    Instrumented test for Login Screen added(Complete)

--- LoginApp/app/src/androidTest/java/com/metacube/chanchal/loginapp/MainActivityTest.java
+++ LoginApp/app/src/androidTest/java/com/metacube/chanchal/loginapp/MainActivityTest.java
@@ -1,45 +1,43 @@
 package com.metacube.chanchal.loginapp;
 
-import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.ActivityInstrumentationTestCase2;
-import android.widget.TextView;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import static org.junit.Assert.*;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.intent.Intents.*;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
 /**
  * Created by Chanchal on 01-03-2016.
  */
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity>{
+public class MainActivityTest {
 
-    MainActivity activity;
-    public MainActivityTest() {
-        super(MainActivity.class);
-    }
+    @Rule
+    public IntentsTestRule<MainActivity> mIntentRule = new IntentsTestRule<MainActivity>(MainActivity.class);
 
-    @Before
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        activity = getActivity();
-    }
 
     @Test
-    public void usernameTextValidator() throws Exception {
-        TextView username= (TextView) activity.findViewById(R.id.uname);
-        assertNotNull("Field cannot be null", username);
+    public void usernameTextValidator() {
+        onView(withId(R.id.uname)).perform(typeText("chanchal"));
+        onView(withId(R.id.uname)).check(matches(withText("chanchal")));
+
+        onView(withId(R.id.pass)).perform(typeText("dabriya"));
+        onView(withId(R.id.pass)).check(matches(withText("dabriya")));
+
+        onView(withId(R.id.button)).perform(click());
+        //intended(toPackage("com.metacube.chanchal.loginapp"));
+        onView(withId(R.id.gridView)).check(matches(isDisplayed()));
     }
 
-    @After
-    @Override
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
 }

63206a5b07b4a0a537c128e18e7e16ec8deba34b
chanchaldabriya7 <chanchal.dabriya@metacube.com>
2016-03-04 11:04:29 +0530

    Made some changes in instrumented test class for login

--- LoginApp/app/src/androidTest/java/com/metacube/chanchal/loginapp/MainActivityTest.java
+++ LoginApp/app/src/androidTest/java/com/metacube/chanchal/loginapp/MainActivityTest.java
@@ -1,7 +1,6 @@
 package com.metacube.chanchal.loginapp;
 
-import android.support.test.espresso.ViewAssertion;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -9,14 +8,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import static org.junit.Assert.*;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.intent.Intents.*;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 /**
  * Created by Chanchal on 01-03-2016.
  */
@@ -24,20 +25,27 @@ import static android.support.test.espresso.assertion.ViewAssertions.*;
 public class MainActivityTest {
 
     @Rule
-    public IntentsTestRule<MainActivity> mIntentRule = new IntentsTestRule<MainActivity>(MainActivity.class);
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
 
     @Test
-    public void usernameTextValidator() {
+    public void usernameTextValidator() throws InterruptedException {
         onView(withId(R.id.uname)).perform(typeText("chanchal"));
+        Thread.sleep(3000);
+        pressBack();
         onView(withId(R.id.uname)).check(matches(withText("chanchal")));
+        Thread.sleep(3000);
 
         onView(withId(R.id.pass)).perform(typeText("dabriya"));
+        closeSoftKeyboard();
+        Thread.sleep(3000);
         onView(withId(R.id.pass)).check(matches(withText("dabriya")));
+        Thread.sleep(3000);
 
         onView(withId(R.id.button)).perform(click());
-        //intended(toPackage("com.metacube.chanchal.loginapp"));
+        Thread.sleep(3000);
         onView(withId(R.id.gridView)).check(matches(isDisplayed()));
+        Thread.sleep(3000);
     }
 
 }
